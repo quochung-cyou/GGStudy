bbb7afae105a17cb316bb877bc188ae1232b05e5
Nguyen Quoc Hung <nguyenquochung.workvn@gmail.com>
2024-08-01 22:26:11 +0700

    feat: resolve jpaauditing

--- backend/src/main/java/web/MainApplication.java
+++ backend/src/main/java/web/MainApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 @EnableEncryptableProperties
 public class MainApplication {
 
